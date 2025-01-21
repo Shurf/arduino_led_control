0e3f8322e8427e642791a7cf36e474926c7207c2
Daniil Yavno <daniil.yavno@veeam.com>
2025-01-21 19:39:59 +0100

    client differentiation support

--- src/main.cpp
+++ src/main.cpp
@@ -4,41 +4,40 @@
 #include <WiFiUdp.h>
 #include <WiFi.h>
 #include <WiFiClientSecure.h>
-#include <Adafruit_NeoPixel.h>
 
 #include "HttpControl.h"
+#include "NeoPixelLed.h"
 #include "secrets.h"
 
-#define USE_NEOPIXEL
 
-#ifdef USE_NEOPIXEL
+//#define NEOPIXEL_LED_COUNT 16
 
-#define NEOPIXEL_LED_COUNT 20
-#define NO_BLANKS true
+// cross
+#define NEOPIXEL_LED_COUNT 357
+
+// ankh
+//#define NEOPIXEL_LED_COUNT 298
+
+// sound strip
+// #define NEOPIXEL_LED_COUNT 88
 
 #define NEOPIXEL_LED_PIN 26
 #define NEOPIXEL_BRIGHTNESS 255
-#define SECTOR_MAX_CYCLES 1
-#define CURRENT_SECTOR_PERCENT_LENGTH 10
+// #define NEOPIXEL_BRIGHTNESS 8
 // #define NEOPIXEL_BRIGHTNESS 32
 
-Adafruit_NeoPixel rgbWS = Adafruit_NeoPixel(NEOPIXEL_LED_COUNT, NEOPIXEL_LED_PIN, NEO_GRB + NEO_KHZ800);
 
-#else
-#define PIN_RED 26
-#define PIN_GREEN 23
-#define PIN_BLUE 27
-#endif
+
+// NeoPixelLed * neoPixelLed = new NeoPixelLed(357, 255, NEOPIXEL_LED_PIN, false); // cross 
+// NeoPixelLed * neoPixelLed = new NeoPixelLed(298, 255, NEOPIXEL_LED_PIN, true); // ankh 
+// NeoPixelLed * neoPixelLed = new NeoPixelLed(24, 8, NEOPIXEL_LED_PIN, false); // circle
+NeoPixelLed * neoPixelLed = new NeoPixelLed(20, 255, NEOPIXEL_LED_PIN, false); // rabbit
 
 #define INTERVAL_MILLIS 1000
 
 WiFiClientSecure client;
 unsigned long lastMillis;
 HttpControl* httpControl;
-
-int currentSectorPercentEnd = 100;
-int currentSectorCycles = 0;
-
 
 void setup() {
   Serial.begin(115200);
@@ -58,19 +57,6 @@ void setup() {
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
 
-  #ifdef USE_NEOPIXEL
-
-  rgbWS.begin();
-  rgbWS.setBrightness(NEOPIXEL_BRIGHTNESS);
-
-  #else
-
-  pinMode(PIN_RED, OUTPUT);
-  pinMode(PIN_GREEN, OUTPUT);
-  pinMode(PIN_BLUE, OUTPUT);
-
-  #endif
-
   lastMillis = millis();
 
   httpControl = new HttpControl();
@@ -78,16 +64,8 @@ void setup() {
   Serial.println("Setup finished");
 }
 
-int getLedIndex(int percentage)
+void loop() 
 {
-  return percentage*NEOPIXEL_LED_COUNT/100 + ((percentage*NEOPIXEL_LED_COUNT % 100) == 0 ? 0 : 1);
-}
-
-void loop() {
-
-  int currentMax = 0;
-
-  String profileName;
 
   unsigned long currentMillis = millis();
   if(currentMillis - lastMillis < INTERVAL_MILLIS && currentMillis >= lastMillis)
@@ -96,65 +74,6 @@ void loop() {
   lastMillis = currentMillis;
 
 
-  #ifdef USE_NEOPIXEL
-
   auto arguments = httpControl->getLedProfileFullParameters();
-
-  if(arguments.useSecondary)
-  {
-    /*int sectorEnd = currentSectorPercentEnd*NEOPIXEL_LED_COUNT/100 + ((currentSectorPercentEnd*NEOPIXEL_LED_COUNT % 100) == 0 ? 0 : 1);
-    int sectorStart = (currentSectorPercentEnd - CURRENT_SECTOR_PERCENT_LENGTH)*NEOPIXEL_LED_COUNT/100 + 
-      (((currentSectorPercentEnd - CURRENT_SECTOR_PERCENT_LENGTH)*NEOPIXEL_LED_COUNT % 100) == 0? 0 : 1);*/
-    int sectorEnd = getLedIndex(currentSectorPercentEnd);
-    int sectorStart = getLedIndex(currentSectorPercentEnd - CURRENT_SECTOR_PERCENT_LENGTH);
-    if(sectorStart < 0)
-      sectorStart = 0;
-    for(int i = 0; i < NEOPIXEL_LED_COUNT; i++)
-    {
-      // < and not <= because sectorEnd contains the starting index of the next block
-      if(i >= sectorStart && i < sectorEnd)
-        rgbWS.setPixelColor(i, arguments.red, arguments.green, arguments.blue);  
-      else
-        rgbWS.setPixelColor(i, arguments.secondaryRed2, arguments.secondaryGreen2, arguments.secondaryBlue2);
-      currentSectorCycles++;      
-    }
-    if(currentSectorCycles >= SECTOR_MAX_CYCLES)
-    {
-      currentSectorCycles = 0;
-      currentSectorPercentEnd -= CURRENT_SECTOR_PERCENT_LENGTH;
-      if(currentSectorPercentEnd <= 0)
-        currentSectorPercentEnd = 100;
-    }
-  }
-  else
-  {
-    currentMax = getLedIndex(arguments.percentage);
-
-    for(int i = 0; i < currentMax; i++)
-      rgbWS.setPixelColor(i, arguments.red, arguments.green, arguments.blue);
-
-    for(int i = currentMax; i < NEOPIXEL_LED_COUNT; i++)
-    {
-#ifdef NO_BLANKS
-      rgbWS.setPixelColor(i, arguments.secondaryRed2, arguments.secondaryGreen2, arguments.secondaryBlue2);
-#else
-      rgbWS.setPixelColor(i, 0, 0, 0);
-#endif
-    }
-  }
-
-  
-
-  rgbWS.show();
-
-  #else
-
-  if(!httpControl->getLedProfileParameters(profileName, red, green, blue, percentage))
-    return;
-
-  analogWrite(PIN_RED, red);
-  analogWrite(PIN_GREEN, green);
-  analogWrite(PIN_BLUE, blue);
-  #endif
-
+  neoPixelLed->display(arguments);
 }
